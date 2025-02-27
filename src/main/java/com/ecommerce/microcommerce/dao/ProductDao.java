24318eadbaa64d52cda84604157a6a93f64d9ffa
laflosh <l_florent_l@hotmail.fr>
2025-02-27 17:14:31 +0100

    add jpa interface

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -1,15 +1,11 @@
 package com.ecommerce.microcommerce.dao;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ecommerce.microcommerce.model.Product;
 
-public interface ProductDao {
-
-	List<Product> findAll();
+public interface ProductDao extends JpaRepository<Product, Integer>{
 	
 	Product findById(int id);
-	
-	Product save(Product product);
 	
 }
