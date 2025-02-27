62b0b285b6ab8a2dda23fea2a0301f41fd1a0c0d
laflosh <l_florent_l@hotmail.fr>
2025-02-27 16:40:30 +0100

    add post mapping for product controller

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
@@ -27,6 +29,13 @@ public class ProductController {
 	public Product afficheUnProduit(@PathVariable int id) {
 		
 		return productDao.findById(id);
+		
+	}
+	
+	@PostMapping("/Product")
+	public void ajouterProduit(@RequestBody Product product) {
+		
+		productDao.save(product);
 		
 	}
 	
