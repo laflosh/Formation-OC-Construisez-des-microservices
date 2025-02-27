2ec468b8f9df1d32d25b587c0bc0337def0c4a5b
laflosh <l_florent_l@hotmail.fr>
2025-02-27 16:14:03 +0100

    implement productdao interface

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,27 +1,32 @@
 package com.ecommerce.microcommerce.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
 
 @RestController
 public class ProductController {
 
+	@Autowired
+	private ProductDao productDao;
+	
 	@GetMapping("/Produits")
-	public String listeProduits() {
+	public List<Product> listeProduits() {
 		
-		return "Un exemple de produit";
+		return productDao.findAll();
 		
 	}
 	
 	@GetMapping("/Produits/{id}")
 	public Product afficheUnProduit(@PathVariable int id) {
 		
-		Product product = new Product(id, new String("Aspirateur"), 100);
-		
-		return product;
+		return productDao.findById(id);
 		
 	}
 	
