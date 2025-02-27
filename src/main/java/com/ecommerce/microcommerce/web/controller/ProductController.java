79244e9d2674c267c4213a0ae1ac411e76fc1495
laflosh <l_florent_l@hotmail.fr>
2025-02-27 16:54:40 +0100

    add response code for post mapping of product controller

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,13 +1,17 @@
 package com.ecommerce.microcommerce.web.controller;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
@@ -33,9 +37,23 @@ public class ProductController {
 	}
 	
 	@PostMapping("/Product")
-	public void ajouterProduit(@RequestBody Product product) {
+	public ResponseEntity<Product> ajouterProduit(@RequestBody Product product) {
 		
-		productDao.save(product);
+		Product productAdded = productDao.save(product);
+		
+		if(Objects.isNull(productAdded)) {
+			
+			return ResponseEntity.noContent().build();
+			
+		}
+		
+		URI location = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(productAdded.getId())
+					.toUri();
+		
+		return ResponseEntity.created(location).build();
 		
 	}
 	
